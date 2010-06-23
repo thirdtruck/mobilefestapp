c32a303018eb678d715c3f0a7ea1df6c730a37ce
James Holder <github.com@thirdtruck.org>
2010-06-23 19:32:50 -0400

    Add Event object.

---
+++ db/migrate/20100623233214_create_events.rb
@@ -0,0 +1,17 @@
+class CreateEvents < ActiveRecord::Migration
+  def self.up
+    create_table :events do |t|
+      t.string :name
+      t.string :date
+      t.string :time
+      t.string :location
+      t.string :cost
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :events
+  end
+end

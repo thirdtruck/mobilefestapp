6e84ea9223b827301a0e46adecb05e3b5c214084
James Holder <github.com@thirdtruck.org>
2010-06-25 14:41:56 -0400

    Replace database contents with actual data.

--- source_data/csv2table.py
+++ source_data/csv2table.py
@@ -5,7 +5,7 @@ import csv
 
 data = sys.stdin.readlines()
 
-c = csv.reader(data, delimiter="\t", quotechar='"')
+c = csv.reader(data, delimiter=",", quotechar='"')
 
 day = ''
 location = ''
@@ -25,3 +25,11 @@ for row in c:
 	print line
 	entry = { 'day': day, 'location': location, 'start': row[0], 'end': row[1], 'band': row[2], 'website': row[3] }
 	entries.append(entry)
+
+print "# ----------- #"
+
+csvOut = csv.writer(open('output.csv', 'w'), delimiter='|', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+rowid = 1
+for entry in entries:
+	csvOut.writerow([rowid, entry['band'], entry['day'], entry['start']+"-"+entry['end'], entry['location'], '', '', '', entry['start'], entry['end']])
+	rowid = rowid+1
